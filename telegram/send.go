60245c93174432dbb83dbe26bb8bf5cd7ad1bacd
janani anbarasan <janani.anbarasan@gmail.com>
2024-07-15 00:45:13 -0500

    fix telegram message  markup formatting

--- telegram/send.go
+++ telegram/send.go
@@ -1,11 +1,42 @@
 package telegram
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"log"
+	"strings"
 
-func Send(bot *tgbotapi.BotAPI, update tgbotapi.Update, reply string){
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func Send(bot *tgbotapi.BotAPI, update tgbotapi.Update, reply string) error {
+	specialCharacterEscaper := strings.NewReplacer(
+		`_`, "\\_",
+		`*`, "\\*",
+		// `[`, "\\[",
+		// `]`, "\\]",
+		// `(`, "\\(",
+		// `)`, "\\)",
+		`~`, "\\~",
+		"`", "\\`",
+		`>`, "\\>",
+		`#`, "\\#",
+		`+`, "\\+",
+		`-`, "\\-",
+		`=`, "\\=",
+		`|`, "\\|",
+		`{`, "\\{",
+		`}`, "\\}",
+		`.`, "\\.",
+		`!`, "\\!",
+	)
+	reply = specialCharacterEscaper.Replace(reply)
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 	msg.ReplyToMessageID = update.Message.MessageID
 	// https://core.telegram.org/bots/api#formatting-options
 	msg.ParseMode = "MarkdownV2"
-	bot.Send(msg)
+	_, err := bot.Send(msg)
+	if err != nil {
+		log.Println("error sending message to bot: " + err.Error())
+		return err
+	}
+	return nil
 }
