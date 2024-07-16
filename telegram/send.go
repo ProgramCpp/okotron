6454ac8388f7bb219233fe5e1b850ceffd28d0c0
janani anbarasan <janani.anbarasan@gmail.com>
2024-07-16 09:25:14 -0500

    use inbuilt utility to escape special characters in telegram markup reply

--- telegram/send.go
+++ telegram/send.go
@@ -2,37 +2,17 @@ package telegram
 
 import (
 	"log"
-	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func Send(bot *tgbotapi.BotAPI, update tgbotapi.Update, reply string) error {
-	specialCharacterEscaper := strings.NewReplacer(
-		`_`, "\\_",
-		`*`, "\\*",
-		// `[`, "\\[",
-		// `]`, "\\]",
-		// `(`, "\\(",
-		// `)`, "\\)",
-		`~`, "\\~",
-		"`", "\\`",
-		`>`, "\\>",
-		`#`, "\\#",
-		`+`, "\\+",
-		`-`, "\\-",
-		`=`, "\\=",
-		`|`, "\\|",
-		`{`, "\\{",
-		`}`, "\\}",
-		`.`, "\\.",
-		`!`, "\\!",
-	)
-	reply = specialCharacterEscaper.Replace(reply)
+	const PARSE_MODE = "MarkdownV2"
+	reply = tgbotapi.EscapeText(PARSE_MODE, reply)
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 	msg.ReplyToMessageID = update.Message.MessageID
 	// https://core.telegram.org/bots/api#formatting-options
-	msg.ParseMode = "MarkdownV2"
+	msg.ParseMode = PARSE_MODE
 	_, err := bot.Send(msg)
 	if err != nil {
 		log.Println("error sending message to bot: " + err.Error())
