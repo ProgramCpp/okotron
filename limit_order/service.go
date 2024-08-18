d228196750aa486a993fa68cebf440705fb2b6c2
janani anbarasan <janani.anbarasan@gmail.com>
2024-08-18 00:10:35 -0500

    implement limit order processor

--- limit_order/service.go
+++ limit_order/service.go
@@ -1,13 +1,21 @@
 package limitorder
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log"
+	"strconv"
 	_ "time"
 
 	cmc "github.com/programcpp/okotron/coin_market_cap"
+	"github.com/programcpp/okotron/db"
+	"github.com/programcpp/okotron/telegram"
+	"github.com/redis/go-redis/v9"
 )
 
 // TODO. implement error channels for async process
+// when the process stops silently, limit orders will no more be processed
 func ProcessOrders() {
 	go func() {
 		for {
@@ -19,7 +27,35 @@ func ProcessOrders() {
 				return
 			}
 
-			_ = prices
+			for token, price := range prices.Tokens {
+
+				priceKey := fmt.Sprintf(db.LIMIT_ORDER_KEY, strconv.Itoa(int(price)))
+				// 0: first element. -1: last element
+				ordersResult := db.RedisClient().LRange(context.Background(), priceKey, 0, -1)
+				if ordersResult.Err() != nil && !errors.Is(ordersResult.Err(), redis.Nil) {
+					log.Println("error fetching limit orders from redis")
+					return
+				} else if errors.Is(ordersResult.Err(), redis.Nil) {
+					// if no orders at this price, move to the next token price
+					continue
+				}
+
+				orders := []telegram.LimitOrderRequestInput{}
+				ordersResult.ScanSlice(orders)
+
+				for _, o := range orders {
+					if o.BuyOrSell == "buy" && o.ToToken == token {
+						processOrder(o)
+					} else if o.BuyOrSell == "sell" && o.FromToken == token {
+						processOrder(o)
+					}
+				}
+			}
 		}
 	}()
+}
+
+
+func processOrder(o telegram.LimitOrderRequestInput){
+
 }
