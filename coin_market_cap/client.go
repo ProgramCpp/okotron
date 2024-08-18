a938701e74f892184d2658e37c0577c8a934e5bc
janani anbarasan <janani.anbarasan@gmail.com>
2024-08-17 23:38:21 -0500

    fix price api

--- coin_market_cap/client.go
+++ coin_market_cap/client.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/programcpp/okotron/utils"
 	"github.com/spf13/viper"
@@ -13,13 +14,19 @@ import (
 )
 
 const (
-	BASE_URL = " https://pro-api.coinmarketcap.com"
+	BASE_URL = "https://pro-api.coinmarketcap.com"
 )
 
 // prices are always in INR. for now, only INR is supported
-type Price struct {
+type PricesData struct {
 	// map from token to price in INR
 	Tokens map[string]float64
+}
+
+func NewPricesData() PricesData {
+	return PricesData{
+		Tokens: make(map[string]float64),
+	}
 }
 
 /*
@@ -31,51 +38,57 @@ https://stackoverflow.com/a/70028568/2508038
 CMC quote api:
 https://coinmarketcap.com/api/documentation/v1/#operation/getV2CryptocurrencyQuotesLatest
 */
-func Prices() (Price, error) {
+func Prices() (PricesData, error) {
 	req, err := http.NewRequest(http.MethodGet, BASE_URL+"/v2/cryptocurrency/quotes/latest", nil)
 	if err != nil {
 		log.Println("error creating cmc quotes http req " + err.Error())
-		return Price{}, err
+		return PricesData{}, err
 	}
 
+	req.Header.Add("X-CMC_PRO_API_KEY", viper.GetString("CMC_KEY"))
+	req.Header.Add("Accept", "application/json")
+
 	// TODO: this is hardcoded for now. get supported tokens from cmc as you support more tokens
-	symbols := ""
+
+	tokenSubset := []string{}
 	for _, token := range utils.SUPPORTED_TOKENS {
+		if !strings.Contains(token, "USD") {
+			tokenSubset = append(tokenSubset, token)
+		}
+	}
+	symbols := ""
+	for _, token := range tokenSubset {
 		symbols += token + ","
 	}
 
 	params := url.Values{}
 	params.Add("symbol", symbols)
-	params.Add("convert", "2796") // this is INR. for now, only INR is supported
-	params.Add("X-CMC_PRO_API_KEY", viper.GetString("CMC_KEY"))
-	params.Add("Accept", "application/json")
+	params.Add("convert", "INR")
 
 	req.URL.RawQuery = params.Encode()
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Println("error making cmc quotes http req " + err.Error())
-		return Price{}, err
+		return PricesData{}, err
 	}
 	resBytes, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Println("error reading cmc quotes response body " + err.Error())
-		return Price{}, err
+		return PricesData{}, err
 	}
 
 	if res.StatusCode != http.StatusOK {
 		// TODO: parse error response
 		log.Println("cmc quotes http req not OK. " + string(resBytes))
-		return Price{}, errors.New("cmc quotes http req not OK")
+		return PricesData{}, errors.New("cmc quotes http req not OK")
 	}
 
-	tokens := gjson.GetMany(string(resBytes), "data.*")
-
-	prices := Price{}
-	for _, t := range tokens {
-		symbol := gjson.Get(t.String(), "symbol")
-		price := gjson.Get(t.String(), "quote.INR.price")
-		prices.Tokens[symbol.String()] = price.Float()
+	prices := NewPricesData()
+	for _, t := range tokenSubset {
+		tokenData := gjson.Get(string(resBytes), "data." + t + ".0") // there is only one currency
+		price := gjson.Get(tokenData.String(), "quote.INR.price")
+		prices.Tokens[t] = price.Float()
 	}
 
 	return prices, nil
--- limit_order/service.go
+++ limit_order/service.go
@@ -1,5 +1,25 @@
 package limitorder
 
-func ProcessOrders(){
+import (
+	"log"
+	_ "time"
 
+	cmc "github.com/programcpp/okotron/coin_market_cap"
+)
+
+// TODO. implement error channels for async process
+func ProcessOrders() {
+	go func() {
+		for {
+			// time.Sleep(5 * 60 * time.Second) // for the free plan, maximum 10K calls per month. poll every 5 minutes
+
+			prices, err := cmc.Prices()
+			if err != nil {
+				log.Println("error fetching cmc prices")
+				return
+			}
+
+			_ = prices
+		}
+	}()
 }
