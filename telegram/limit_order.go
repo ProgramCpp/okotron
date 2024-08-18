db50b582b05d2de9fb78160573ac5665233e1b23
janani anbarasan <janani.anbarasan@gmail.com>
2024-08-17 20:58:09 -0500

    fix limit order telegram command

--- telegram/limit_order.go
+++ telegram/limit_order.go
@@ -18,13 +18,14 @@ import (
 // wow! limit order also supports swaps together, and across networks!🔥
 // limit order is essentially swap at a certain target price
 type LimitOrderRequestInput struct {
-	BuyOrSell   bool   `json:"buy_or_sell" redis:"limit-order/buy-or-sell"`
-	FromToken   string `json:"from_token" redis:"limit-order/from-token"`
-	FromNetwork string `json:"from_network" redis:"limit-order/from-network"`
-	ToToken     string `json:"to_token" redis:"limit-order/to-token"`
-	ToNetwork   string `json:"to_network" redis:"limit-order/to-network"`
-	Quantity    string `json:"quantity" redis:"limit-order/quantity"`
-	Price       string `json:"price" redis:"limit-order/price"`
+	// valid values are "buy" and "sell"
+	BuyOrSell   string `json:"buy_or_sell" redis:"/limit-order/buy-or-sell"`
+	FromToken   string `json:"from_token" redis:"/limit-order/from-token"`
+	FromNetwork string `json:"from_network" redis:"/limit-order/from-network"`
+	ToToken     string `json:"to_token" redis:"/limit-order/to-token"`
+	ToNetwork   string `json:"to_network" redis:"/limit-order/to-network"`
+	Quantity    string `json:"quantity" redis:"/limit-order/quantity"`
+	Price       string `json:"price" redis:"/limit-order/price"`
 }
 
 func (l LimitOrderRequestInput) MarshalBinary() (data []byte, err error) {
@@ -239,7 +240,7 @@ func LimitOrderQuantityInput(bot *tgbotapi.BotAPI, update tgbotapi.Update, isBac
 			log.Printf("error encountered when saving sub command key price command. %s", err.Error())
 			Send(bot, update, "something went wrong. try again.")
 		}
-		LimitOrderCallback(bot, update)
+
 		return
 	}
 	// handle first digit of quantity. redis returns Nil error of the field is not found
@@ -329,5 +330,6 @@ func LimitOrderCallback(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	}
 
 	limitOrderKey := fmt.Sprintf(db.LIMIT_ORDER_KEY, r.Price)
-	db.RedisClient().RPush(context.Background(),limitOrderKey, r)
+	// todo: handle error
+	db.RedisClient().RPush(context.Background(), limitOrderKey, r)
 }
